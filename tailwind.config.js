1e64f3746d6ccdfc824096dbd607913ab608eb77
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2021-10-30 19:41:16 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./**/*.liquid'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height: {
+        94: '22rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
